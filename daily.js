6403f07873bfaeb9fdb1a3c0d786da4577e92837
Mackenzie <mackmiller19@gmail.com>
2018-12-07 13:23:30 -0500

    legend

--- daily.js
+++ daily.js
@@ -136,10 +136,10 @@ var svg = dimple.newSvg("#chartContainer4","100%", "100%");
      new dimple.color("#57575F", "#57575F", 1), // red
      new dimple.color("#CA3542", "#CA3542", 1)
  ];
-myChart.addLegend(60, 10, 500, 20, "right");
+
  myChart.setMargins("30%", "10px", "30%", "20px");
 x.timeInterval = 4;
-myChart.addLegend(500, 10, 500, 20, "center");
+
  	//s.lineMarkers=true;
    myChart.draw(1500);
 
